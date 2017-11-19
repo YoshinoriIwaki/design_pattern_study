b760dfa2c32d34b2f2f79aa728bb0c54f22e9603
YoshinoriIwaki <tanlab@hotmail.co.jp>
2017-11-19 14:42:35 +0900

    ファクトリーメソッドパターンの勉強

--- src/template_method/WoodCutPrint.java
+++ src/template_method/WoodCutPrint.java
@@ -1,5 +1,17 @@
 package template_method;
 
-public class WoodCutPrint {
+public abstract class WoodCutPrint {
+	private Cuttable hanzai = new Wood();
+
+	public abstract void draw(Cuttable hanzai);
+	public abstract void cut(Cuttable hanzai);
+	public abstract void print(Cuttable hanzai);
+	public void createWoodCutPrint() {
+		Wood hanzai = new Wood();
+
+		draw(hanzai);
+		cut(hanzai);
+		print(hanzai);
+	}
 
 }
