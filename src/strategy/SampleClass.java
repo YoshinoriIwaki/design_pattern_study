4e5ce9f972aac59e552162472c74e1548e9d13a3
YoshinoriIwaki <tanlab@hotmail.co.jp>
2017-11-22 00:06:57 +0900

    strategyパターンの勉強

---
+++ src/strategy/SampleClass.java
@@ -0,0 +1,34 @@
+package strategy;
+
+public class SampleClass {
+	
+	private int type = -1;
+	public static final int COMPARE_AGE = 1;
+	public static final int COMPARE_HEIGHT = 2;
+	public static final int COMPARE_WEIGHT = 3;
+	
+	public SampleClass(int compareType) {
+		this.type = type;
+	}
+	
+	public int compare(Human h1, Human h2) {
+		if(type == COMPARE_AGE) {
+			if(h1.age > h2.age) {
+				return 1;
+			} else if(h1.age == h2.age) {
+				return 0;
+			} else {
+				return -1;
+			}
+		} else if(type == COMPARE_HEIGHT) {
+			if(h1.height > h2.height) {
+				return 1;
+			} else if(h1.height == h2.height) {
+				return 0;
+			} else {
+				return -1;
+			}
+		}
+		return 2;
+	}
+}
